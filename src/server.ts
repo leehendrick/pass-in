c74dc4f7d67d49cc389805ee7cec6a4d21a6fbe0
leehendrick <imhendrick16@gmail.com>
2024-04-03 17:48:42 +0100

    api: add post method

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,34 @@
 import fastify from "fastify";
+import z from "zod";
+import { PrismaClient } from '@prisma/client';
+
 
 const app = fastify();
-
-app.get('/', () => {
-    return 'Hello NLW Unite'
+const prisma = new PrismaClient({
+    log: ['query'],
 })
+app.post('/events', async (request, reply) => {
+    const createEventSchema = z.object({
+        title: z.string().min(10),
+        details: z.string().nullable(),
+        maximumAttendeess: z.number().int().positive().nullable(),
+    })
+
+    const data = createEventSchema.parse(request.body)
+
+    const event = await prisma.event.create({
+        data: {
+            title: data.title,
+            details: data.details,
+            maximumAttendees: data.maximumAttendeess,
+            slug: new Date().toISOString()
+        }
+    })
+
+    return reply.status(201).send({ eventId: event.id })
+})
+
+
 
 app.listen({ port: 4002 }).then(() => {
     console.log('[-----SERVER RUNNING-----]')
