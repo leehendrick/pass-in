ef0f52912bd23d15da158e503b45ac27a298cb99
leehendrick <imhendrick16@gmail.com>
2024-04-03 23:43:00 +0100

    ref-api: refactoring post route to a new file

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,14 @@
 import fastify from "fastify";
-import z from "zod";
-import { PrismaClient } from '@prisma/client';
+import { serializerCompiler, validatorCompiler,  } from 'fastify-type-provider-zod';
+import {createEvent} from "./routes/create-event";
 
 
 const app = fastify();
-const prisma = new PrismaClient({
-    log: ['query'],
-})
-app.post('/events', async (request, reply) => {
-    const createEventSchema = z.object({
-        title: z.string().min(10),
-        details: z.string().nullable(),
-        maximumAttendeess: z.number().int().positive().nullable(),
-    })
 
-    const data = createEventSchema.parse(request.body)
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-    const event = await prisma.event.create({
-        data: {
-            title: data.title,
-            details: data.details,
-            maximumAttendees: data.maximumAttendeess,
-            slug: new Date().toISOString()
-        }
-    })
-
-    return reply.status(201).send({ eventId: event.id })
-})
-
-
+app.register(createEvent);
 
 app.listen({ port: 4002 }).then(() => {
     console.log('[-----SERVER RUNNING-----]')
