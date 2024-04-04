354d716b45f4550cc57d4a85aebd814871af5b6c
leehendrick <imhendrick16@gmail.com>
2024-04-04 23:24:44 +0100

    dep: conf swagger for doc

--- src/routes/create-event.ts
+++ src/routes/create-event.ts
@@ -3,6 +3,7 @@ import z from "zod";
 import {generateSlug} from "../utils/generate-slug";
 import {prisma} from "../lib/prisma";
 import {FastifyInstance} from "fastify";
+import {BadRequest} from "./_errors/bad-request";
 
 
 export async function createEvent(app: FastifyInstance) {
@@ -10,6 +11,8 @@ export async function createEvent(app: FastifyInstance) {
         .withTypeProvider<ZodTypeProvider>()
         .post('/events', {
             schema: {
+                summary: 'Create an event',
+                tags: ['events'],
                 body: z.object({
                     title: z.string().min(10),
                     details: z.string().nullable(),
@@ -39,7 +42,7 @@ export async function createEvent(app: FastifyInstance) {
             })
 
             if (eventWithSameSlug !== null) {
-                throw new Error('Another event with same title  already exists.')
+                throw new BadRequest('Another event with same title  already exists.')
             }
 
             const event = await prisma.event.create({
