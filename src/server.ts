cb2e9a98ea9f794c7b1ab758a7fbb27d12b889dd
leehendrick <imhendrick16@gmail.com>
2024-04-04 19:19:49 +0100

    api: add new routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import fastify from "fastify";
 import { serializerCompiler, validatorCompiler,  } from 'fastify-type-provider-zod';
 import {createEvent} from "./routes/create-event";
+import {registerForEvent} from "./routes/register-for-event";
+import {getEvent} from "./routes/get-event";
+import {getAttendeeBadge} from "./routes/get-attendee-badge";
 
 
 const app = fastify();
@@ -9,6 +12,9 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent);
+app.register(registerForEvent);
+app.register(getEvent);
+app.register(getAttendeeBadge);
 
 app.listen({ port: 4002 }).then(() => {
     console.log('[-----SERVER RUNNING-----]')
